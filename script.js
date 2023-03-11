64058a2b748fa9ab3d9fb55df4a7a1e3c46b8da0
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-03-11 00:07:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var diasTrabalhandoLarissa = 0
 var diasTrabalhandoMarcella = 0
 var diasTrabalhandoPaulo = 0
 
+//Print em tela dos dias trabalhados
 document.getElementById('trabBreno').innerHTML = diasTrabalhandoBreno;
 document.getElementById('trabEduardo').innerHTML = diasTrabalhandoEduardo;
 document.getElementById('trabLarissa').innerHTML = diasTrabalhandoLarissa;
@@ -12,10 +13,56 @@ document.getElementById('trabPaulo').innerHTML = diasTrabalhandoPaulo
 
 // Função para criar uma div dinamicamente e adicionar o funcionario dentro dessa div, selecionando funcionario, dia e função e caso essa div ja exista ele sera incluido dentro da div existente
 function adicionar(){
+  const loja = document.getElementById('lojaSelected')
   const funcionario = document.getElementById('funcionarios').value
   const dia = document.getElementById('diaSelecionado').value
   const funcao = document.getElementById('funcao').value
+  if(funcionario == 'default' || dia == 'default' || funcao == 'default' || loja == 'default'){
+    alert('Todas as caixas devem estar preenchidas para a criação das tabelas!')
+    return
+  }
 
+  if(funcionario == 'rose' && funcao !== 'gerente' ){
+    alert(`${funcionario.toUpperCase()} só pode ser selecionado na função: "GERENTE"`)
+    return
+  }
+
+  if(funcionario !== 'rose' && funcao == 'gerente'){
+    alert(`Apenas "ROSE" pode ser selecionada como "${funcao.toUpperCase()}"`)
+    return
+  }
+
+  // Verifica se o funcionário trabalhou nos últimos 6 dias
+  // Se sim, impossibilita ele de trabalhar novamente e alerta um erro
+  // const ultimosDias = document.querySelectorAll('.diaAdicionado')
+  // let contadorDiasFunc = 0
+
+  // // Seleciona apenas os últimos 6 dias
+  // const ultimosSeisDias = Array.prototype.slice.call(ultimosDias, -6)
+  // if (ultimosSeisDias.length > 0) {
+  //   const ultimaDiv = ultimosDias[ultimosDias.length - 1]
+  //   const funcao = ultimaDiv.querySelector('.pFinal').value
+  //   console.log(funcao.value)
+  //   if (funcao === 'folga') {
+  //     contadorDiasFunc = 0
+  //   }
+  // }  
+
+  // for (let i = 0; i < ultimosSeisDias.length; i++) {
+  //   const divDia = ultimosSeisDias[i]
+  //   const funcionarioS = divDia.querySelector(`#${funcionario+divDia.id}`)
+  //   if (funcionarioS !== null) {
+  //     contadorDiasFunc++
+  //     if (contadorDiasFunc === 6) {
+  //       const funcao = document.getElementById('funcao').value
+  //       if (funcao !== 'folga') {
+  //         alert(`O funcionário ${funcionario.toUpperCase()} trabalhou nos últimos 6 dias, então só é possível atribuir a função 'FOLGA'.`)
+  //         return
+  //       }
+  //     }
+  //   }
+  // }
+  
   switch(funcionario){
     case 'breno':
       if(funcao !== 'folga') diasTrabalhandoBreno ++
@@ -46,7 +93,7 @@ function adicionar(){
   const divExistente = document.getElementById(dia)
   if (divExistente !== null){
     const pFinal = document.createElement('p')
-    pFinal.textContent = `FUNCIONARIO: ${funcionario.toUpperCase()} / FUNÇÃO: ${funcao.toUpperCase()}`
+    pFinal.textContent = `${funcionario.toUpperCase()} / ${funcao.toUpperCase()}`
 
     divExistente.appendChild(pFinal)
     pFinal.setAttribute('id', funcionario+dia)
@@ -59,10 +106,21 @@ function adicionar(){
 
     const pFinal = document.createElement('p')
     pFinal.classList.add('pFinal')
-    pFinal.textContent = `FUNCIONARIO: ${funcionario.toUpperCase()} / FUNÇÃO: ${funcao.toUpperCase()}`
+    pFinal.textContent = `${funcionario.toUpperCase()} / ${funcao.toUpperCase()}`
 
+    const checkDomingo = document.getElementById('checkDomingo');
+    const checkFeriado = document.getElementById('checkFeriado');
     const paragrafoDia = document.createElement('p')
-    paragrafoDia.textContent = `DIA: ${dia.toUpperCase()}`
+
+    if(checkDomingo.checked && checkFeriado.checked){
+      paragrafoDia.textContent = `DIA: ${dia.toUpperCase()} / "DOMINGO/FERIADO"`
+    }else if(checkDomingo.checked){
+      paragrafoDia.textContent = `DIA: ${dia.toUpperCase()} / "DOMINGO"`
+    }else if(checkFeriado.checked){
+      paragrafoDia.textContent = `DIA: ${dia.toUpperCase()} / "FERIADO"`
+    }else{
+      paragrafoDia.textContent = `DIA: ${dia.toUpperCase()}`
+    }
 
     div.appendChild(paragrafoDia)
     div.appendChild(pFinal)
@@ -72,12 +130,16 @@ function adicionar(){
 
 // Função remover container do card dentro do container do mes, selecionando apenas o dia
 function removerDia() {
-  const funcionario = document.getElementById('funcionarios').value
   const dia = document.getElementById('diaSelecionado').value;
   const card = document.getElementById(dia);
   const container = document.getElementById('containerTabelaMes');
   const funcao = document.getElementById('funcao').value;
   
+  //Parar a função caso on inputs necessarios nao teham sido preenchidos
+  if(dia == 'default'){
+    alert('A opção dia precisa estar preenchida e existir para ser removida.')
+    return
+  }
   // Array com os nomes dos funcionários
   const funcionarios = ['breno', 'eduardo', 'larissa', 'marcella', 'paulo'];
 
@@ -125,6 +187,9 @@ function removerDia() {
 function removerFuncionario() {
   const funcionario = document.getElementById('funcionarios').value
   const dia = document.getElementById('diaSelecionado').value
+  if(dia == 'default' || funcionario == 'default'){
+    alert('As opções "Funcionario" e "Dia", precisam estar preenchidas e existir para que sejam removidas.')
+  }
   const paragrafo = document.getElementById(funcionario + dia)
   const funcao = document.getElementById('funcao').value
   if (paragrafo) {
@@ -156,15 +221,100 @@ function removerFuncionario() {
   }
 }
 
+//Função para copiar a ultima Div criada dinamicamente e criar uma div identica após ela mudando apenas o dia para o dia selecionado pelo usuario
+function copiarUltimaDiv() {
+  const ultimoDia = document.querySelector('#containerTabelaMes .diaAdicionado:last-child');
 
+  if (ultimoDia !== null) {
+    const dia = document.getElementById('diaSelecionado').value;
+    if (document.getElementById(dia) !== null) {
+      alert(`O dia ${dia} já foi adicionado. Por favor, selecione um dia diferente.`);
+      return;
+    }
+    const novoDia = ultimoDia.cloneNode(true);
+    novoDia.setAttribute('id', dia);
+
+    const checkDomingo = document.getElementById('checkDomingo');
+    const checkFeriado = document.getElementById('checkFeriado');
+    if(checkDomingo.checked && checkFeriado.checked){
+      novoDia.querySelector('p:first-child').textContent = `DIA: ${dia.toUpperCase()} / "DOMINGO/FERIADO"`;
+    }else if(checkDomingo.checked){
+      novoDia.querySelector('p:first-child').textContent = `DIA: ${dia.toUpperCase()} / "DOMINGO"`;
+    }else{
+      novoDia.querySelector('p:first-child').textContent = `DIA: ${dia.toUpperCase()}`;
+    }
+
+
+    const paragrafos = novoDia.querySelectorAll('.pFinal');
+    paragrafos.forEach((p) => {
+      const idOriginal = p.getAttribute('id');
+      p.setAttribute('id', idOriginal.replace(/\d+$/, dia));
+    });
+    const container = document.getElementById('containerTabelaMes');
+    const divExistente = document.getElementById(dia);
+    if (divExistente !== null) {
+      divExistente.parentNode.replaceChild(novoDia, divExistente);
+    } else {
+      container.appendChild(novoDia);
+    }
+
+    // Array com os nomes dos funcionários
+    const funcionarios = ['breno', 'eduardo', 'larissa', 'marcella', 'paulo'];
+
+    // Loop para iterar sobre todos os funcionários
+    for (let i = 0; i < funcionarios.length; i++) {
+      const nome = funcionarios[i];
+      const cardAtual = document.getElementById(nome + dia);
+      
+      // Verifica se o funcionário está presente no card clonado
+      if (cardAtual !== null) {
+
+        // Incrementa o contador de dias trabalhados do funcionário
+        switch (nome) {
+          case 'breno':
+            diasTrabalhandoBreno ++;
+            document.getElementById('trabBreno').innerHTML = diasTrabalhandoBreno;
+            break;
+          case 'eduardo':
+            diasTrabalhandoEduardo ++;
+            document.getElementById('trabEduardo').innerHTML = diasTrabalhandoEduardo;
+            break;
+          case 'larissa':
+            diasTrabalhandoLarissa ++;
+            document.getElementById('trabLarissa').innerHTML = diasTrabalhandoLarissa;
+            break;
+          case 'marcella':
+            diasTrabalhandoMarcella ++;
+            document.getElementById('trabMarcella').innerHTML = diasTrabalhandoMarcella;
+            break;
+          case 'paulo':
+            diasTrabalhandoPaulo ++;
+            document.getElementById('trabPaulo').innerHTML = diasTrabalhandoPaulo;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+}
+
+//Função para ao clicar no botao baixar um pdf com a escala completa 
 document.getElementById("download-pdf-btn").addEventListener("click", function() {
+  const loja = document.getElementById('lojaSelected').value
   const doc = new jsPDF();
-  const diasTrabalhando = `BREN0: ${diasTrabalhandoBreno}, EDUARDO: ${diasTrabalhandoEduardo}, LARISSA${diasTrabalhandoLarissa}, MARCELA: ${diasTrabalhandoMarcella}, PAULO: ${diasTrabalhandoPaulo}`
+  const diasTrabalhando = `BREN0: ${diasTrabalhandoBreno}, EDUARDO: ${diasTrabalhandoEduardo}, LARISSA: ${diasTrabalhandoLarissa}, MARCELA: ${diasTrabalhandoMarcella}, PAULO: ${diasTrabalhandoPaulo}`;
   const conteudo = document.getElementById("containerTabelaMes");
-  doc.text(diasTrabalhando, 20, 10);
-  doc.fromHTML(conteudo, 20, 10);
-  doc.save("Escala.pdf");
+  
+  // adiciona o título e configura a fonte
+  doc.setFontSize(16);
+  doc.text(`ESCALA MENSAL LOJA ${loja}`, 105, 20, {align: "center"});
+
+  doc.setFontSize(14);
+  doc.fromHTML(diasTrabalhando, 20, 40, {align: "center"});
+  
+  // configura a fonte para o tamanho do conteúdo
+  doc.setFontSize(12);
+  doc.fromHTML(conteudo, 20, 50, {align: "center"});
+  doc.save("escala.pdf")
 });
-
-
-
